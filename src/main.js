bd89e71dadcaa174e8b155f701eec5fbdfdda3c7
falltints <2832731037@qq.com>
2018-06-19 22:39:38 +0800

    initialize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+import store from './store'
+
+import 'common/stylus/index.styl'
+
+fastClick.attach(document.body)
+
+Vue.use(VueLazyLoad, {
+  loading: require('common/image/default.png')
+})
 
 Vue.config.productionTip = false
 
@@ -8,5 +19,6 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
